3d46cadba37e22a256e585c239418dfabdcd4895
Matt Bunch <matt_bunch@hms.harvard.edu>
2021-10-10 18:32:06 -0400

    adding various template pages

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,17 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/documentation")
+def documentation():
+    return render_template("documentation.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
