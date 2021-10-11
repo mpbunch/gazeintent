a75ad3f54ec74a09129af10c69bb06f92dc7e579
Matt Bunch <matt_bunch@hms.harvard.edu>
2021-10-10 21:44:21 -0400

    fixing conflict

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,26 @@
 from flask import request
 import joblib
 from flask import Flask, render_template
+from models import db, User
+from forms import UsersForm
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/usersdb' 
+db.init_app(app)
+app.secret_key = "e14a-key" 
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/documentation")
+def documentation():
+    return render_template("documentation.html")
+
+@app.route("/register", methods=['GET', 'POST'])
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
