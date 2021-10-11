f9863803a1d9f22fb4a2c78fdd9f5c6fd5192ac0
yihan-zhou <195493347@qq.com>
2021-10-10 17:52:58 -0700

    add signup page

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,20 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
