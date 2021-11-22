7cbdfeb51a288686cb1d1fca255335ab3182736a
yihan-zhou <195493347@qq.com>
2021-11-21 22:20:42 -0800

    profile page update and change password

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, PasswordField
+from wtforms import StringField, IntegerField, SubmitField, PasswordField, validators
 from wtforms.validators import DataRequired
 
+
 class UsersForm(FlaskForm):
-    email_address = StringField('Email Address', validators=[DataRequired()])
+    email_address = StringField('Email Address',[DataRequired()])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired()])
+    repeat_new_password = PasswordField('Repeat New Password', validators=[DataRequired()])
     gender = StringField('Gender', validators=[])
     age = IntegerField('Age', validators=[])
     zipcode = IntegerField('Zipcode', validators=[])
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 # import joblib
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_login import current_user, LoginManager, logout_user, login_required, login_user
-
 from models import db, User, Calibration
 from forms import UsersForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
 import requests
 import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 # In order to keep the database credentials, and secret_key secure
 # We need to use environment variables
 # Heroku does this natively
@@ -29,17 +29,21 @@ secret_key = os.environ.get('SECRET_KEY')
 
 if not db_url or not secret_key:
     # Fail if no environment variable is found.
-    raise Exception('Credentials not found. Did you for get to export environment variables?')
+    raise Exception(
+        'Credentials not found. Did you for get to export environment variables?')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres://','postgresql://')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL').replace('postgres://', 'postgresql://')
 app.secret_key = secret_key
 login_manager = LoginManager()
 login_manager.init_app(app)
 db.init_app(app)
 
 # This will be the calibration api
-@app.route('/api/calibrate', methods=['POST','GET'])
+
+
+@app.route('/api/calibrate', methods=['POST', 'GET'])
 def api_calibrate():
     if request.method == 'POST':
         # We can now take the payload and insert it into the db
@@ -49,29 +53,32 @@ def api_calibrate():
         user_id = current_user.id
         print('--')
         print(current_user)
-        new_calibration = Calibration(user_id=user_id, data=json.dumps(payload))
+        new_calibration = Calibration(
+            user_id=user_id, data=json.dumps(payload))
         try:
             db.session.add(new_calibration)
             db.session.commit()
             message = "Calibration Complete" if payload['id'] == 9 else "Calibration data saved."
             message = {
-                "message":message,
-                "type":1
+                "message": message,
+                "type": 1
             }
         except Exception as e:
             print(e)
             db.session.rollback()
             message = {
                 "message": "Opps, something went wrong. Try again.",
-                "type":2
+                "type": 2
             }
         return message
-    active="calibrate"
+    active = "calibrate"
     return render_template("calibrate.html", active=active)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('shared/404.html'), 404
+
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -79,6 +86,7 @@ def user_loader(user_id):
     if not user:
         return
     return user
+
 
 @login_manager.request_loader
 def request_loader(request):
@@ -89,33 +97,38 @@ def request_loader(request):
             return
         return user
 
+
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return redirect(url_for('index'))
+
 
 @app.route('/client')
 @app.route('/admin')
 @login_required
 def admin():
     if current_user.role == 'admin':
-        active="admin"
+        active = "admin"
         data = Calibration.query.all()
 
-        return render_template("admin.html", user=current_user, active=active, calibration=data)
-    active="client"
+        return render_template("admin/admin.html", user=current_user, active=active, calibration=data)
+    active = "client"
     return render_template("client/client.html", user=current_user, active=active)
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route("/")
 def index():
-    active="index"
+    active = "index"
     return render_template("site/index.html", active=active)
 
-@app.route("/profile", methods=['GET', 'POST', 'PUT'])
+
+@app.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
     form = UsersForm()
@@ -123,29 +136,81 @@ def profile():
         "message": False,
         "type": 0
     }
+    print(current_user.password)
+
     if request.method == "POST":
-        if request.form.get("submit") == "update":
+
+        # Update bio section
+        if request.form.get("bio") == "updatebio":
+            current_user.first_name = form.first_name.data
+            current_user.last_name = form.last_name.data
+            current_user.email_address = form.email_address.data
+            db.session.commit()
+            message = {
+                "message": "Successfuly updated bio.",
+                "type": 1
+            }
+
+        # Update demo section
+        elif request.form.get("demo") == "updatedemo":
             current_user.age = form.age.data
             current_user.gender = form.gender.data
             current_user.zipcode = form.zipcode.data
             db.session.commit()
-            print("Post profile information")
+            message = {
+                "message": "Successfuly updated demo.",
+                "type": 1
+            }
+
+        # Update password
+        elif request.form.get("security") == "updatesecurity":
+            new_password=request.form['new_password']
+            repeat_new_password=request.form['repeat_new_password']
+
+            # if current password is correct
+            if current_user.check_password(form.current_password.data):
+                # if passwords match
+                if new_password == repeat_new_password:
+                    # Hash the new password
+                    current_user.password=generate_password_hash(new_password)
+                    db.session.commit()
+                    message = {
+                        "message": "Password has been updated.",
+                        "type": 1
+                    }
+                # if passwords don't match
+                else:
+                    # remind to check
+                    message = {
+                        "message": "Please make sure your new passwords match.",
+                        "type": 2
+                    }
+            # if current password is not correct
+            else:
+                message = {
+                    "message": "Current password is wrong.",
+                    "type": 2
+                }
+
         else:
-            pass
-    elif request.method == "GET":
-        form.age.data = current_user.age
-        form.gender.data = current_user.gender
-        form.zipcode.data = current_user.zipcode
-        print("Get profile information")
+            form.first_name.data = current_user.first_name
+            form.last_name.data = current_user.last_name
+            form.email_address.data = current_user.email_address
+            form.age.data = current_user.age
+            form.gender.data = current_user.gender
+            form.zipcode.data = current_user.zipcode
+            print("Get profile information")
+
     active = "profile"
     return render_template("client/profile.html", form=form, message=message, user=current_user, active=active)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    message={
-        "message":False,
-        "type":0
+    message = {
+        "message": False,
+        "type": 0
     }
     if request.method == 'POST':
         email_address = request.form['email_address']
@@ -154,36 +219,41 @@ def login():
             login_user(user)
             return redirect(url_for('admin'))
         else:
-            message={
-                "message":"Invalid credentials, try again.",
-                "type":2
+            message = {
+                "message": "Invalid credentials, try again.",
+                "type": 2
             }
-    active="login"
+    active = "login"
     return render_template("site/login.html", form=form, message=message, active=active)
+
 
 @app.route("/documentation")
 def documentation():
-    active="documentation"
+    active = "documentation"
     return render_template("documentation.html", active=active)
+
 
 @app.route("/calibrate")
 @login_required
 def clientcalibrate():
-    active="calibrate"
+    active = "calibrate"
     return render_template("client/calibrate.html", active=active)
+
 
 @app.route("/test")
 @login_required
 def clienttest():
-    active="test"
+    active = "test"
     return render_template("client/test.html", active=active)
+
 
 @app.route("/clienthistory")
 @login_required
 def clienthistory():
-    active="clienthistory"
+    active = "clienthistory"
     user_id = current_user.get_id()
-    data = Calibration.query.with_entities(Calibration.data).filter_by(user_id=user_id).all()
+    data = Calibration.query.with_entities(
+        Calibration.data).filter_by(user_id=user_id).all()
     data = [json.loads(x[0]) for x in data]
     print(data)
     return render_template("client/history.html", active=active, history=data)
@@ -200,27 +270,29 @@ def clienthistory():
 #     # show the form, it wasn't submitted
 #     return render_template('signup.html')
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = UsersForm()
-    message={
-        "message":False,
-        "type":0
+    message = {
+        "message": False,
+        "type": 0
     }
 
     if form.validate_on_submit():
         email_address = request.form['email_address']
         first_name = request.form['first_name']
         last_name = request.form['last_name']
-        new_user = User(first_name=first_name, last_name=last_name, email_address=email_address)
+        new_user = User(first_name=first_name,
+                        last_name=last_name, email_address=email_address)
         # hash the password
         new_user.set_password(request.form['password'])
         try:
             db.session.add(new_user)
             db.session.commit()
             message = {
-                "message":"Successfuly created account.",
-                "type":1
+                "message": "Successfuly created account.",
+                "type": 1
             }
         except:
             # Not sure this is needed
@@ -230,10 +302,11 @@ def signup():
             db.session.rollback()
             message = {
                 "message": "Opps, something went wrong. Try again.",
-                "type":2
+                "type": 2
             }
-    active="signup"
+    active = "signup"
     return render_template('site/signup.html', form=form, message=message, active=active)
+
 
 @app.route('/slack', methods=['post'])
 def slack():
@@ -243,14 +316,16 @@ def slack():
     try:
         status = data['data']['status']
         action = data['action']
-        data = {"text": '{action} gazeintent. Status: {status}'.format(action=action, status=status)}
+        data = {"text": '{action} gazeintent. Status: {status}'.format(
+            action=action, status=status)}
         slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
     except Exception:
         return "Malformed payload."
 
     try:
         if action.lower() == 'update':
-            response = requests.post(slack_webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+            response = requests.post(slack_webhook_url, data=json.dumps(
+                data), headers={'Content-Type': 'application/json'})
             if response.status_code != 200:
                 pass
                 # raise ValueError(f'Request to slack returned an error {response.status_code}, the response is:\n{response.text}')
