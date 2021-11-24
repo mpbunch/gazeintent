44e513de0e9785e433d45a3c826e6ed3bf37bac7
geo-ml <54856877+geo-ml@users.noreply.github.com>
2021-11-24 14:41:20 -0600

    updating client test js

--- app/static/js/client_test.js
+++ app/static/js/client_test.js
@@ -2,28 +2,30 @@
 // ml_util is work in progress
 // copied and edited from mb_utils
 
-import { doGaze } from "./ml_util.js";
+import { doGazeTest } from "./ml_util.js";
 
 (function () {
-    let gaze = new doGaze();
+    let gaze = new doGazeTest();
+    console.log('testingtesgin!!!!!!!!!!!!!!!!!!!!!!!!!');
     // Show loading wheel wile webgazer is being loaded
     gaze.loading();
     // wait for webgazer before calibration grid load
     var checkExist = setInterval(function () {
         // Once the eye location div is loaded, show calibration or test
         if (document.getElementById('webgazerGazeDot')) {
-            console.log('Webgazer Loaded!');
+            console.log('Webgazer Loaded! --1');
             // turn off loading wheel
             gaze.loading(false);
             // init test
             let test = gaze.test();
+            console.log('starting test now')
             // start test
             test.start();
             // clear interval, so only one test event is loaded
             clearInterval(checkExist);
         }
     }, 100);
-})(doGaze);
+})(doGazeTest);
 
 /**
 // Create grid using gazeCell
--- app/static/js/ml_util.js
+++ app/static/js/ml_util.js
@@ -2,14 +2,14 @@
 // for client test grid, cells and symbols
 // work in progress, much yet to do
 
-export class doGaze {
+export class doGazeTest {
     constructor(...grid) {
         this.grid = grid
     }
 
     #makeGrid = (gridSize, type = 'test') => {
         console.log('Make Grid');
-        console.log(gridSize);
+        console.log('Grid Size: ' + gridSize);
         const grid = document.querySelector('#gazeGrid');
 
         if (grid) {
@@ -38,10 +38,9 @@ export class doGaze {
                 if (type === 'test') {
                     console.log('type: test')
 
-                    this.center_element = document.createElement('div');
-                    this.center_element.className = 'gazeGrid gaze-cell gazeCellTest';
+                    this.center_element = document.createElement('button');
+                    this.center_element.className = 'gazeCellTest';
                     this.center_element.id = `gazeCellTest-${i}`;
-
 
                     this.center_element.addEventListener('click', () => {
                         console.log('Click');
@@ -69,7 +68,7 @@ export class doGaze {
             loading.innerHTML = '<div id="webgazerLoading"><div class="spinner-box"><div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div></div></div>';
             grid.append(loading);
         } else {
-            console.log('not loading')
+            console.log('not loading --1')
             let loading = document.getElementById('webgazerLoading');
             if (loading) loading.remove()
         }
@@ -113,210 +112,40 @@ export class doGaze {
         return { start: start, end: end }
     }
 
-    // 
     test = (gridSize = 9) => {
-        console.log('Test');
-
+        console.log('test');
+        var helpers = {}
         // Make calibration Grid
         if (gridSize) this.#makeGrid(gridSize);
         let accuracy = document.createElement('div');
         accuracy.id = 'accuracy'
         document.getElementById('gazeGrid').parentElement.append(accuracy)
-        const start = () => {
-            console.log('Test Start');
+        // Define calibration start function
+        const start = (cellId = 1) => {
+            helpers.advance(cellId);
+            console.log('##############TEST Start');
         };
+
+        // Define calibration stop funciton
         const end = () => {
-            console.log('Test Stop');
+            console.log('#############TEST Stop');
         };
-        return { start: start, end: end }
-    }
 
-}
+        helpers.compare = (target, tracker) => {
 
-export function calibrateAdvance(id) {
-    // Click event will change to 'red dot event'
-    // let calibrated = false;
-    // let calibrateCount = 0;
-    // let dot = document.getElementById('webgazerGazeDot');
-    // let target = document.getElementById(id)
-    // while (!calibrated) {
-    //     console.log(calibrateCount)
-    //     if (calibrateCount > 5) calibrated = overlaps(source, dot)
-    //     calibrateCount += 1
-    // }
-    // let elements = document.getElementsByClassName('gazeCell');
-    // Array.from(elements).forEach(element => {
-    //     let button = element.getElementsByTagName('button')[0];
-    //     button.disabled = true;
-    //     if (element.id == id) button.disabled = false;
-    // });
-}
-
-function calibrateDestory() {
-    // Maybe a calibration complete message
-    // and then destroy
-    // .. will also need to do something about the overflow:hidden (no scroll)
-    document.getElementById('gazeContainer').innerHTML = '<h3>Calibration Complete</h3>'
-}
-
-
-
-/*
- * Sets store_points to true, so all the occuring prediction
- * points are stored
- */
-function store_points_variable() {
-    webgazer.params.storingPoints = true;
-}
-
-/*
- * Sets store_points to false, so prediction points aren't
- * stored any more
- */
-function stop_storing_points_variable() {
-    webgazer.params.storingPoints = false;
-}
-
-/**
-* This function occurs on resizing the frame
-* clears the canvas & then resizes it (as plots have moved position, can't resize without clear)
-*/
-function resize() {
-    var canvas = document.getElementById('client-calibrate');
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-window.addEventListener('resize', resize, false);
-
-/*
- * This function calculates a measurement for how precise 
- * the eye tracker currently is which is displayed to the user
- */
-function calculatePrecision(past50Array) {
-    var windowHeight = window.innerHeight;
-    var windowWidth = window.innerWidth;
-
-    // Retrieve the last 50 gaze prediction points
-    var x50 = past50Array[0];
-    // offset for header height
-    var y50 = past50Array[1] - 30;
-
-    // Calculate the position of the point the user is staring at
-    var staringPointX = windowWidth / 2;
-    var staringPointY = windowHeight / 2;
-
-    var precisionPercentages = new Array(50);
-    calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY);
-    var precision = calculateAverage(precisionPercentages);
-
-    // Return the precision measurement as a rounded percentage
-    return Math.round(precision);
-};
-
-/*
- * Calculate percentage accuracy for each prediction based on distance of
- * the prediction point from the centre point (uses the window height as
- * lower threshold 0%)
- */
-function calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY) {
-    for (let x = 0; x < 50; x++) {
-        // Calculate distance between each prediction and staring point
-        var xDiff = staringPointX - x50[x];
-        var yDiff = staringPointY - y50[x];
-        var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
-
-        // Calculate precision percentage
-        var halfWindowHeight = windowHeight / 2;
-        var precision = 0;
-        if (distance <= halfWindowHeight && distance > -1) {
-            precision = 100 - (distance / halfWindowHeight * 100);
-        } else if (distance > halfWindowHeight) {
-            precision = 0;
-        } else if (distance > -1) {
-            precision = 100;
         }
 
-        // Store the precision
-        precisionPercentages[x] = precision;
+        // Define calibration advance function
+        helpers.advance = (cellId) => {
+            console.log('ADVANCE')
+            let elements = document.getElementsByClassName('gazeCell');
+            Array.from(elements).forEach(element => {
+                let button = element.getElementsByTagName('button')[0];
+                button.disabled = true;
+                if (element.id == cellId) button.disabled = false;
+            });
+        }
+
+        return { start: start, end: end }
     }
-}
-
-/*
- * Calculates the average of all precision percentages calculated
- */
-function calculateAverage(precisionPercentages) {
-    var precision = 0;
-    for (let x = 0; x < 50; x++) {
-        precision += precisionPercentages[x];
-    }
-    precision = precision / 50;
-    return precision;
-}
-
-/**
- * Restart the calibration process by clearing the local storage and reseting the calibration point
- */
-function Restart() {
-    document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
-    webgazer.clearData();
-    ClearCalibration();
-    PopUpInstruction();
-}
-
-/**
- * Show the Calibration Points
- */
-function ShowCalibrationPoint() {
-    $(".Calibration").show();
-    $("#Pt5").hide(); // initially hides the middle button
-}
-
-/**
-* This function clears the calibration buttons memory
-*/
-function ClearCalibration() {
-    // Clear data from WebGazer
-
-    $(".Calibration").css('background-color', 'red');
-    $(".Calibration").css('opacity', 0.2);
-    $(".Calibration").prop('disabled', false);
-
-    CalibrationPoints = {};
-    PointCalibrate = 0;
-}
-
-// sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-
-var overlaps = (function () {
-    function getPositions(elem) {
-        var pos, width, height;
-        pos = elem.getBoundingClientRect();
-        width = elem.offsetWidth / 2;
-        height = elem.offsetHeight;
-        return [[pos.left, pos.left + width], [pos.top, pos.top + height]];
-    }
-
-    function comparePositions(p1, p2) {
-        console.log(p1, p2);
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function (a, b) {
-        var elem_a, elem_b;
-        elem_a = document.getElementById(a);
-        elem_b = document.getElementById(b);
-        if (!elem_a || !elem_b) return false
-        var pos1 = getPositions(elem_a),
-            pos2 = getPositions(elem_b);
-        return comparePositions(pos1[0], pos2[0]) && comparePositions(pos1[1], pos2[1]);
-    };
-})();
+};
