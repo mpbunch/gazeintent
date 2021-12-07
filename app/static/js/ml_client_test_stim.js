d78ce1c7f299c68195ce8e315949a41f630afefc
geo-ml <54856877+geo-ml@users.noreply.github.com>
2021-12-06 18:11:34 -0600

    updated stimulus symbols, color and grid random selections

--- app/static/js/ml_client_test_stim.js
+++ app/static/js/ml_client_test_stim.js
@@ -4,13 +4,17 @@ var timeStopTest = '';  // test stop time (same time as the end of final symbol 
 var timeStartStim = ''; // time the stimulus appears in the cell
 var timeStopStim = '';  // time the stimulus disappears from the screen - either by timeout or by user gazing at active cell
 
-var randomSymbol = '';  // random symbol list
-var randomColor = '';   // random color list
-var stimSymbol = '';    // single symbol for now, update to a list of 10 symbols
+var symbolList = Array("fas fa-circle", "fas fa-square", "fas fa-heart", "fas fa-star");  // list of symbols
+var colorList = Array("#ff0000", "#00ff00", "#0000ff");  // list of colors
+var randomSymbol = '';  // randomly selected symbol
+var randomColor = '';   // randomly selected color
+var randomCell = '';   // randomly selected grid cell
+var stimSymbol = '';    // single symbol for dev testing, update to a list of 10 symbols
 
 var stimData = []       // dictionary for the stim symbol, color, time and user data points
 
 var stimEventsTotal = 4; // total stimulus events in the test?
+var stimCount = 0;      // Current count of the number of times a the stimulus placement function has run
 
 
 // checkExist copied from webcam.js and edited for the client test page
@@ -55,56 +59,63 @@ function btnStartStim() {
 
 // Stimulus function - places a random symbol in a random cell for a random duration
 function stim() {
-    console.log('this is the STIM function');
-    
-    // how many times will the stim function run
-
-
-    // randomly select a stimulus symbol and color
-    console.log('randomly selecting a stim symbol and color...');
-//    randomSymbol = "CIRCLE";
-//    randomColor = "RED";
-
-    stimSymbol = '<i class="fas fa-circle"></i>';
-
-    // randomly select a cell id
-    console.log('randomly selecting a cell...');
-    // var testCellId = '';
-    
-    // get the div of the randomly selected cell and add the symbol
-    console.log('placing symbol into the randomly selected cell...');
-    var el = document.getElementById("gaze-1").innerHTML = stimSymbol;
-
-    // record the symbol start time
-    timeStartStim = Date.now();
-
-    // wait the randomly selected amount of time, then remove stim symbol
-    console.log('start stim display time...')
-    setTimeout(() => {
-        console.log("stop stim display time...");
+    for (let i = 0; i < 5; i++) {
+        stimCount++;
+        console.log('=============this is the STIM function cycle: #' + stimCount);
         
-        // remove the symbol
-        el = document.getElementById("gaze-1").innerHTML = '---';
-    }, 3000);   // PLACEHOLDER TO SIMULATE RANDOM DELAY TIME
-    
-    // record the symbol stop time
-    // if user gaze touches the active cell
-    timeStopStim = Date.now();
+        // randomly select a stimulus symbol and color
+        console.log('randomly selecting a stim symbol and color...');
+        
+        // random cell
+        randomCell = "gaze-" + Math.floor(Math.random()*9+1);
+        console.log("randomly selected cell: " + randomCell);
+        
+        // random symbol
+        randomSymbol = symbolList[Math.floor(Math.random()*symbolList.length)];
+        console.log("randomly selected symbol: " + randomSymbol);
+        
+        // random color
+        randomColor = colorList[Math.floor(Math.random()*colorList.length)];
+        console.log("randomly selected color: " + randomColor);
+        
+        // combine random symbol and color into the stimulus symbol
+        var stimSymbol = "<i class=" + '"' + randomSymbol + '" ' + " style=" + '"' + "color:" + randomColor + '"' + "></i>";
+        console.log("stimSymbol: " + stimSymbol);
 
-    // if symbol duration time runs out before user gaze touch active cell
-    timeStopStim = Date.now();
+        // get the div of the randomly selected cell and add the symbol
+        var el = document.getElementById(randomCell)
+        el.innerHTML = stimSymbol;
 
+        // record the symbol start time
+        timeStartStim = Date.now();
+        console.log('stim display start time: ' + timeStartStim);
 
+        // wait the selected amount of time, then remove stim symbol
+        setTimeout(() => {
+            console.log("stim display end time");
+            // remove the symbol
+            console.log("------============>>>>> REMOVING SYMBOL");
+            el.innerHTML = '---';
 
-    // save/append stim data to a dictionary
-    console.log('saved stimulus event details into a dictionary');
+        }, 2000);   // PLACEHOLDER TO SIMULATE RANDOM DELAY TIME
 
-    // APPEND TO stimData
+        // record the symbol stop time
+        // if user gaze touches the active cell
+        timeStopStim = Date.now();
 
-    stimData = [{"meta":{"start":1111,"end":2222},"data":[{"cell":2, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20},{"cell":9, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20},{"cell":4, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20}]}]    // PLACEHOLDER DATA 
+        // if symbol duration time runs out before user gaze touch active cell
+        timeStopStim = Date.now();
 
-    // write dictionary to database
-    console.log(stimData);
+        // save/append stim data to a dictionary
+        console.log('saved stimulus event details into a dictionary ****************************');
+
+        // APPEND TO stimData
+
+        stimData = [{"meta":{"start":1111,"end":2222},"data":[{"cell":2, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20},{"cell":9, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20},{"cell":4, "stim":"square", "start":1111, "end":2222, "gaze":1100, "color":"red", "latency":20}]}]    // PLACEHOLDER DATA 
+
+        // write dictionary to database
+        console.log("stimData Array --> " + stimData);
+    }
 }
 
 
